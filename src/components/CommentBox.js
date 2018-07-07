800e9552a7fe48c6579b3030310569b63fea618d
thinktwice13 <marioo.krajacic@gmail.com>
2018-07-07 18:11:24 +0200

    Add event handlers to CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,3 +1,24 @@
 import React from 'react';
 
-export default () => <div>Comment Box</div>;
+class CommentBox extends React.Component {
+  state = {comment: ""}
+  handleChange = e => this.setState({comment: e.target.value})
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({comment: ""})
+    console.log(this.state.comment)
+  }
+  render() {
+    return <form>
+      <h4>Add a Comment</h4>
+      <textarea 
+      value={this.state.comment}
+      onChange={this.handleChange}/>
+      <div>
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>
+    </form>
+  }
+}
+
+export default CommentBox;
--- src/components/__tests__/App.js
+++ src/components/__tests__/App.js
@@ -5,6 +5,11 @@ import App from "components/App"
 import CommentBox from "components/CommentBox"
 import CommentList from "components/CommentList"
 
+let wrapped
+
+beforeEach(() => {
+  wrapped = shallow(<App/>)
+})
 
 it("shows a comment box", () => {
   // const div = document.createElement("div")
@@ -15,12 +20,9 @@ it("shows a comment box", () => {
 
   // ReactDOM.unmountComponentAtNode(div);
 
-  const wrapped = shallow(<App/>)
-
   expect(wrapped.find(CommentBox).length).toEqual(1)
 })
 
 it("Shows a comment list", () => {
-  const wrapped = shallow(<App />)
   expect(wrapped.find(CommentList).length) .toEqual(1)
 })
