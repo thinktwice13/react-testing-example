59db18428dc8db268fb26d1aea13a59a880ceeaf
thinktwice13 <marioo.krajacic@gmail.com>
2018-07-07 12:33:00 +0200

    Test CommentList with Enzyme

--- src/components/__tests__/App.js
+++ src/components/__tests__/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom"
 import { shallow } from "enzyme"
 import CommentBox from "../CommentBox"
+import CommentList from "../CommentList"
 
 import App from "../App"
 
@@ -17,4 +18,9 @@ it("shows a comment box", () => {
   const wrapped = shallow(<App/>)
 
   expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it("Shows a comment list", () => {
+  const wrapped = shallow(<App />)
+  expect(wrapped.find(CommentList).length) .toEqual(1)
 })
